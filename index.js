cef5f48c80389792dbd4559ca3d80f425dc33dc2
Elvis Osuji <97410294+BigVeezus@users.noreply.github.com>
2022-11-04 09:23:12 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import {franc} from 'franc'
+import langs from 'langs'
+import colors from 'colors'
+const input = process.argv[2];
+const langCode = franc(input)
+
+if (langCode === 'und'){
+    console.log("SORRY IT WASNT FOUND!".red)
+} else {
+    const language = langs.where("3", langCode)
+console.log(`Our best guess is ${language.name}`.green);
+}
